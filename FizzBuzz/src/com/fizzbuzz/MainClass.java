1e7137c37889c414eb244d988fd0f4f4b0267125
FiroRos <ofir.workmode@gmail.com>
2024-07-16 10:59:00 +0300

    closed scanner

--- FizzBuzz/src/com/fizzbuzz/MainClass.java
+++ FizzBuzz/src/com/fizzbuzz/MainClass.java
@@ -4,20 +4,21 @@ import java.util.Scanner;
 
 public class MainClass {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Choose a number: ");
-        int number = scanner.nextInt();
-        
-        if (number % 5 == 0 && !(number % 3 ==  0))
-            System.out.println("Fizz");
+        try(Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Choose a number: ");
+            int number = scanner.nextInt();
             
-        else if (number % 3 == 0 && !(number % 5 == 0))
-            System.out.println("Buzz");
+            if (number % 5 == 0 && !(number % 3 ==  0))
+                System.out.println("Fizz");
+                
+            else if (number % 3 == 0 && !(number % 5 == 0))
+                System.out.println("Buzz");
 
-        else if (number % 5 == 0 && number % 3 == 0)
-            System.out.println("FizzBuzz");
+            else if (number % 5 == 0 && number % 3 == 0)
+                System.out.println("FizzBuzz");
 
-        else
-            System.out.println(number);
+            else
+                System.out.println(number);
+        }
     }
  }
